85edc897c86a8f3df1651f5d0651c85b60366b91
OmarTahoun <omaraiz489@gmail.com>
2019-01-26 17:24:23 +0300

    No repetetion Keeping track with the Log file

--- scrap.py
+++ scrap.py
@@ -1,37 +1,55 @@
 import os
 import flickrapi
 import urllib
+import fnmatch
 
 
 KEY = "dd31b95032ac547830155e09cb33eea1"
 SECRET = "0b32af0505818a2b"
 
 flickr=flickrapi.FlickrAPI(KEY,SECRET,cache=True)
+f = open("log.txt", 'r')
+urls = [x[:-1] for x in f]
+f.close()
+
 def flickr_walk(keyword):
-    path = keyword+"/"
-    if not os.path.exists(path):
-        os.makedirs(path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    if not os.path.exists(folder+"/"+keyword):
+        os.makedirs(folder+"/"+keyword)
+    path = folder+"/"+keyword+"/"
 
-
+    count = len(fnmatch.filter(os.listdir(path), "*.jpeg"))
 
     i = 1
     photos = flickr.walk(text=keyword, tags=keyword, extras='url_c', per_page=100, sort='relevance')
+    f = open("log.txt", "a")
     for photo in photos:
         if i <= number:
-            name =  keyword +" "+str(i)
-            filePath = path + name
+            name =  keyword +" "+str(count)
+            filePath = path + name+".jpeg"
+
             try:
                 url=photo.get('url_c')
-                urllib.request.urlretrieve(url, filePath)
-                print("Saving "+ name)
-                i += 1
+
+                if url not in urls:
+                    f.write(url+"\n")
+                    urllib.request.urlretrieve(url, filePath)
+                    print("Saving " + keyword + " "+ str(i))
+                    i += 1
+                    count += 1
+                else:
+                    continue
+
             except Exception as e:
                 print("ERROR: " + str(e))
         else:
             break
+    f.close()
 
 keyWords = input().split(' ')
 number = int(input())
+folder = input()
 
 
 for keyWord in keyWords:

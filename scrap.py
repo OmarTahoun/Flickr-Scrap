5467948ff6b261eadd47b44f1e12b3dea0765880
OmarTahoun <omaraiz489@gmail.com>
2019-01-26 14:59:58 +0300

    Creates folders for each key word

--- scrap.py
+++ scrap.py
@@ -1,16 +1,21 @@
+import os
 import flickrapi
 import urllib
 
 
 KEY = "dd31b95032ac547830155e09cb33eea1"
 SECRET = "0b32af0505818a2b"
-path = "test/"
 
 flickr=flickrapi.FlickrAPI(KEY,SECRET,cache=True)
 def flickr_walk(keyword):
-    photos = flickr.walk(text=keyword, tags=keyword, extras='url_c', per_page=100, sort='relevance')
+    path = keyword+"/"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
 
     i = 1
+    photos = flickr.walk(text=keyword, tags=keyword, extras='url_c', per_page=100, sort='relevance')
     for photo in photos:
         if i <= number:
             name =  keyword +" "+str(i)
@@ -25,8 +30,10 @@ def flickr_walk(keyword):
         else:
             break
 
-keyWord = input()
+keyWords = input().split(' ')
 number = int(input())
-flickr_walk(keyWord)
-print("Done!!")
-print("Downloaded: "+str(number) +" Photos Scuccesfully")
+
+
+for keyWord in keyWords:
+    flickr_walk(keyWord)
+    print("Downloaded: "+str(number) + " " +keyWord +" Photos Scuccesfully")

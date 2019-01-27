6051c86dbf1adb627b91cbb2d66050f34944aa93
OmarTahoun <omaraiz489@gmail.com>
2019-01-27 15:39:21 +0300

    Better user interaction

--- scrap.py
+++ scrap.py
@@ -4,13 +4,7 @@ import urllib
 import fnmatch
 
 
-KEY = "dd31b95032ac547830155e09cb33eea1"
-SECRET = "0b32af0505818a2b"
 
-flickr=flickrapi.FlickrAPI(KEY,SECRET,cache=True)
-f = open("log.txt", 'r')
-urls = [x[:-1] for x in f]
-f.close()
 
 def flickr_walk(keyword):
     if not os.path.exists(folder):
@@ -22,8 +16,8 @@ def flickr_walk(keyword):
     count = len(fnmatch.filter(os.listdir(path), "*.jpeg"))
 
     i = 1
-    photos = flickr.walk(text=keyword, tags=keyword, extras='url_c', per_page=100, sort='relevance')
     f = open("log.txt", "a")
+    photos = flickr.walk(text=keyword, tags=keyword, extras='url_c', per_page=100, sort='relevance')
     for photo in photos:
         if i <= number:
             name =  keyword +" "+str(count)
@@ -32,7 +26,7 @@ def flickr_walk(keyword):
             try:
                 url=photo.get('url_c')
 
-                if url not in urls:
+                if url not in urls and url != None:
                     f.write(url+"\n")
                     urllib.request.urlretrieve(url, filePath)
                     print("Saving " + keyword + " "+ str(i))
@@ -47,9 +41,34 @@ def flickr_walk(keyword):
             break
     f.close()
 
-keyWords = input().split(' ')
-number = int(input())
+
+print()
+print("Enter Your API key: ", end='')
+KEY = input().strip()
+
+
+print("Enter Your API Secret: ", end='')
+SECRET = input().strip()
+flickr=flickrapi.FlickrAPI(KEY,SECRET,cache=True)
+
+
+
+print()
+print("Enter the directory to save the photos in: ", end='')
 folder = input()
+
+print("Enter photos tags to download: ", end='')
+keyWords = input().strip().split(' ')
+
+
+print("Enter how many photos do you want to download: ", end='')
+number = int(input())
+print()
+
+
+f = open("log.txt", 'r')
+urls = [x[:-1] for x in f]
+f.close()
 
 
 for keyWord in keyWords:

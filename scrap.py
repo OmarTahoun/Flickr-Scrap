cde6f7c06ecf289624816acbb76e9f6e95fe1be5
OmarTahoun <omaraiz489@gmail.com>
2019-06-16 22:16:20 +0300

    Reformating in progress

--- scrap.py
+++ scrap.py
@@ -2,6 +2,8 @@ import os
 import flickrapi
 import urllib
 import fnmatch
+import json
+import argparse
 
 
 
@@ -42,35 +44,103 @@ def flickr_walk(keyword):
     f.close()
 
 
-print()
-print("Enter Your API key: ", end='')
-KEY = input().strip()
+def set_key_secret():
+    key = input("KEY: ")
+    secret = input("SECRET: ")
+    with open(".secret", "w") as file:
+        cred = {'Key': key, 'Secret': secret}
+        json.dump(cred, file)
+
+def get_key_secret():
+    with open(".secret", "r") as file:
+        data = file.read()
+        creds = json.loads(data)
+        key = creds['Key']
+        secret = creds['Secret']
+        return [key, secret]
+
+def check_key_secret():
+    creds = get_key_secret()
+    key = creds[0]
+    secret = creds[1]
+
+    if key and secret:
+        return [key, secret]
+    else:
+        False
+
+def check_path(location, keyword):
+    if not os.path.exists(location):
+        os.makedirs(location)
+    if not os.path.exists(location+"/"+keyword):
+        os.makedirs(location+"/"+keyword)
+    path = location+"/"+keyword+"/"
+    return path
+
+def get_images(flickr, keyword, path, count):
+    # with open('log.json', 'r') as file:
+    #     data = file.read()
+    #     downloaded_images = json.loads(data)
 
 
-print("Enter Your API Secret: ", end='')
-SECRET = input().strip()
-flickr=flickrapi.FlickrAPI(KEY,SECRET,cache=True)
+    URLS = flickr.walk(text=keyword, tags=keyword, extras='url_c', per_page=100, sort='relevance')
+    photo_id = len(fnmatch.filter(os.listdir(path), "*.jpeg"))
+    for url in URLS:
+        if count > 0:
+            name =  keyword +" "+str(photo_id)
+            filePath = path + name+".jpeg"
+
+            try:
+                image = url.get('url_c')
+
+                if image != None:
+                    urllib.request.urlretrieve(image, filePath)
+                    print("Saving " + keyword + " "+ str(photo_id))
+                    count -=1
+                    photo_id +=1
+                else:
+                    continue
+
+            except Exception as e:
+                print("ERROR: " + str(e))
+        else:
+            break
 
 
+def main():
+    parser = argparse.ArgumentParser(description="Flickr")
+    sub_commands = parser.add_subparsers(help='Choose what command to run',dest='choice')
 
-print()
-print("Enter the directory to save the photos in: ", end='')
-folder = input()
+    parser_auth = sub_commands.add_parser('auth', help='Add or Update the Authentication Credentials')
+    parser_auth.add_argument('-key', dest='key', help='API KEY', required = True)
+    parser_auth.add_argument('-secret', dest='secret', help='API Secret', required = True)
 
-print("Enter photos tags to download: ", end='')
-keyWords = input().strip().split(' ')
+    parser_scrap = sub_commands.add_parser('fetch', help='Download pictures with a keyword')
+    parser_scrap.add_argument('-keyword', dest='keyword', help='the keyword to look for', required=True)
+    parser_scrap.add_argument('-path', dest='path', help='the location to save the pictures', required=True)
+    parser_scrap.add_argument('-count', dest='count', help='the number of pictures to download', type = int ,required=True)
 
+    arguments = parser.parse_args()
+    choice = arguments.choice
 
-print("Enter how many photos do you want to download: ", end='')
-number = int(input())
-print()
+    if choice == 'auth':
+        key = arguments.key
+        secret = arguments.secret
+        set_key_secret(key, secret)
+    else:
+        keyword = arguments.keyword
+        location = arguments.path
+        count = arguments.count
 
+        key, secret = check_key_secret()
+        if key and secret:
+            flickr = flickrapi.FlickrAPI(key,secret,cache=True)
+            path = check_path(location, keyword)
+            get_images(flickr, keyword, path, count)
 
-f = open("log.txt", 'r')
-urls = [x[:-1] for x in f]
-f.close()
+        else:
+            print("Wrong / NO API Authentication details were Found, To Update / Set Your API Authentication details")
+            print("run 'scrap.py auth -key <API key> -secret <API secret>'")
 
-
-for keyWord in keyWords:
-    flickr_walk(keyWord)
-    print("Downloaded: "+str(number) + " " +keyWord +" Photos Scuccesfully")
+if __name__ == '__main__':
+    main()
